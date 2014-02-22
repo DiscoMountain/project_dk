740966d0ef2ca8d06e52b06d23306ba6df04c53e
exezive <exezive@gmail.com>
2014-02-22 17:39:08 +0100

    migrated to scaltra

---
+++ src/main/scala/discomountain/app/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package discomountain.app
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends Project_dkStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
