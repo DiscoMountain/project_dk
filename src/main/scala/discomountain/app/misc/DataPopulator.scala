1fcb32ebb3447bae81c97c515fd67bfe80554a8e
exezive <exezive@gmail.com>
2014-03-27 09:03:04 +0100

    adding a user as well

--- src/main/scala/discomountain/app/misc/DataPopulator.scala
+++ src/main/scala/discomountain/app/misc/DataPopulator.scala
@@ -2,7 +2,6 @@ package discomountain.app.misc
 
 
 import com.mongodb.casbah.Imports._
-import discomountain.app.{SolarSystem, ObjectDistance, Sun, Planet}
 
 /**
  * Can be used to populate the database with the start data
@@ -10,19 +9,38 @@ import discomountain.app.{SolarSystem, ObjectDistance, Sun, Planet}
 object DataPopulator extends App {
 
   val mongoClient = MongoClient("localhost", 27017)("DiscoKings")
-  val planet1 = MongoDBObject("name" -> "p1",
-    "radius" -> 7)
 
-  val planet2 = MongoDBObject("name" -> "p2",
-    "radius" -> 6)
+  mongoClient.getCollection("solarsystems").drop()
+  mongoClient.getCollection("players").drop()
 
-  val planet3 = MongoDBObject("name" -> "p3", "radius" -> 10)
+  addSolarSystem()
+  addUser()
 
-  val sun = MongoDBObject("name" -> "Sun",
-    "radius" -> 30,
-    "planets" -> List((planet1, 70), (planet2, 60), (planet3, 110)))
-  val system = MongoDBObject("name" -> "xz",
-    "sun" -> sun)
-  mongoClient.getCollection("solarsystems").insert(system)
+  def addSolarSystem() = {
+    val planet1 = MongoDBObject("name" -> "p1",
+      "radius" -> 7)
+
+    val planet2 = MongoDBObject("name" -> "p2",
+      "radius" -> 6)
+
+    val planet3 = MongoDBObject("name" -> "p3", "radius" -> 10)
+
+    val sun = MongoDBObject("name" -> "Sun",
+      "radius" -> 30,
+      "planets" -> List((planet1, 70), (planet2, 60), (planet3, 110)))
+    val system = MongoDBObject("name" -> "xz",
+      "sun" -> sun)
+    mongoClient.getCollection("solarsystems").insert(system)
+  }
+
+  def addUser() = {
+    val startPosition = MongoDBObject("solarsystem" -> "xz",
+      "planet" -> "p2")
+
+    val defaultUser = MongoDBObject("name" -> "Alpha",
+      "startPosition" -> startPosition)
+
+    mongoClient.getCollection("players").insert(defaultUser)
+  }
 
 }
